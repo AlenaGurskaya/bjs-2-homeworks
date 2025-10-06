30315a3cd213a134521f92f16cb7d77c8427bc1e
Alena Gurskaya <alena.gurskaya@gmail.com>
2025-10-06 10:51:57 +1000

    Work 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,28 +1,29 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = (b ** 2) - (4 * a * c);
-  if (d === 0) {
-    let root = -b / (2 * a);
-    arr.push(root);
-  } else if (d > 0) {
-    let rootOne = ((-b + Math.sqrt(d)) / (2 * a));
-    let rootTwo = ((-b - Math.sqrt(d)) / (2 * a));
-    arr.push(rootOne);
-    arr.push(rootTwo);
-  } 
-  return arr;
+	let arr = [];
+	let d = (b ** 2) - (4 * a * c);
+	if (d === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+	} else if (d > 0) {
+		let rootOne = ((-b + Math.sqrt(d)) / (2 * a));
+		let rootTwo = ((-b - Math.sqrt(d)) / (2 * a));
+		arr.push(rootOne);
+		arr.push(rootTwo);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let monthPercent = percent / 100 / 12;
-  let sumReturn = amount - contribution;
+	let monthPercent = percent / 100 / 12;
+	let sumReturn = amount - contribution;
 
-  if (sumReturn <= 0) {
-    return 0;
-  }
+	if (sumReturn <= 0) {
+		return 0;
+	}
 
-  let monthPay = sumReturn * (monthPercent + (monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1)));
-  let sum = monthPay * countMonths;
-  return parseFloat(sum.toFixed(2));
+	let monthPay = sumReturn * (monthPercent + (monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1)));
+	let sum = monthPay * countMonths;
+	return parseFloat(sum.toFixed(2));
 }
