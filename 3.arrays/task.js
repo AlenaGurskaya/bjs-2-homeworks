6c461fa564ee7b99064106f787fb4268ec454e16
Alena Gurskaya <alena.gurskaya@gmail.com>
2025-10-10 09:24:53 +1000

    Work 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
+//Задача 1
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length === arr2.length) {
+		return arr1.every((element, index) => element === arr2[index]);
+	}
+	return false;
 }
 
+//Задача 2
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, arr) => {
+		acc += item;
+		if (index === arr.length - 1) {
+			return acc / arr.length;
+		}
+		return acc;
+	}, 0)
+	return result;
 }
